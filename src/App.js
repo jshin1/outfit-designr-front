7cfb5e6af82597ee544aedf826ec013ee979c048
Ji Shin <j2yshin@gmail.com>
2019-04-01 00:23:30 -0400

    Create landing page w/ background image, title, and tagline

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import './App.scss';
 
 import { BrowserRouter as Router, Route, Link, Redirect } from 'react-router-dom';
+import LandingPage from './containers/LandingPage';
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <LandingPage/>
       </div>
     );
   }
---
+++ src/containers/LandingPage.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class LandingPage extends Component {
+
+  render() {
+    return (
+      <div className='landing-page'>
+        <h1>Outfit Designr</h1>
+        <div>Creating a timeless outfit is easy. We'll show you how.</div>
+      </div>
+    );
+  }
+
+}
+
+export default LandingPage;
