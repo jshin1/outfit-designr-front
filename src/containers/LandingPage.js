035f967c7c1e701ceaf736b3480475d5b0742751
Ji Shin <j2yshin@gmail.com>
2019-04-05 16:00:34 -0400

    Create sign up form

--- src/containers/LandingPage.js
+++ src/containers/LandingPage.js
@@ -5,8 +5,7 @@ class LandingPage extends Component {
   render() {
     return (
       <div className='landing-page'>
-        <div className='image-one'></div>
-        <div className='tagline'>{"Creating a timeless outfit is easy. We'll show you how."}</div>
+
         <div className='landing-page-bar'>
           <h1>Outfit Designr</h1>
 
@@ -15,8 +14,28 @@ class LandingPage extends Component {
             <input placeholder='Password'></input>
             <button>Sign In</button>
           </div>
+        </div>
 
+        <div className='signup'>
+          <div className='tagline'>{"Creating a timeless outfit is easy. We'll show you how."}</div><br/>
+          First name
+          <input></input><br/>
+          Last name
+          <input></input><br/>
+          Username
+          <input></input><br/>
+          Password
+          <input></input><br/>
 
+          <div className='agreement'>
+            {"By clicking 'Join Now', you agree to be awesome."}
+          </div><br/>
+
+          <button>Join Now</button>
+        </div>
+
+        <div className='sitemap'>
+        Sitemap
         </div>
       </div>
     );
